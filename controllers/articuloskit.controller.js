251734a8ef48a46413fe23516ae197c70af79892
Carlos Rocha <ing_rocha@hotmail.com>
2021-01-12 14:54:21 -0700

    Data base

---
+++ controllers/articuloskit.controller.js
@@ -0,0 +1,135 @@
+const db = require("../models");
+const articuloskit = db.articuloskit;
+
+exports.create = (req, res) => {
+  const Kit = {
+    articulos_id: req.body.articulos_id,
+    usuarios_id: req.body.usuarios_id,
+    articulos: req.body.articulos,
+    cantidad: req.body.cantidad,
+  };
+
+  articuloskit
+    .create(Kit, { usuario: req.usuario })
+    .then((data) => {
+      res.send({ articulokit: data });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.errors || "Sucedio un error al crear!",
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  articuloskit
+    .findAll()
+    .then((data) => {
+      res.send({ articuloskit: data });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Sucedio un error al obtener!",
+      });
+    });
+};
+
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  articuloskit
+    .findByPk(id)
+    .then((data) => {
+      data.pass = "";
+      res.send({ articulo: data });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error al buscar id=" + id,
+      });
+    });
+};
+
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+  // console.log(req.body);
+  articuloskit
+    .update(
+      req.body,
+      {
+        where: { id: id },
+        individualHooks: true,
+      },
+      { usuario: req.usuario }
+    )
+    .then((num) => {
+      // console.log(num.length);
+      if (num == 1) {
+        res.send({
+          message: "Se actualizo correctamente.",
+        });
+      } else {
+        res.send({
+          message: `No se pudo actualizar id=${id}!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error al actualizar id=" + id,
+      });
+    });
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  articuloskit
+    .destroy(
+      {
+        where: { id: id },
+        individualHooks: true,
+      },
+      { usuario: req.usuario }
+    )
+    .then((num) => {
+      console.log(num);
+      if (num == 1) {
+        res.send({
+          message: "Fue borrado correctamente!",
+        });
+      } else {
+        res.send({
+          message: `No se pudo borrar id=${id}!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error al borra id=" + id,
+      });
+    });
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+  articuloskit
+    .destroy(
+      {
+        where: {},
+        truncate: false,
+      },
+      { usuario: req.usuario }
+    )
+    .then((nums) => {
+      res.send({ message: `${nums} elementos fueron borrados!` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error al borrar los elementos",
+      });
+    });
+};
